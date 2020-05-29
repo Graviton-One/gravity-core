bea193e0b1ef8eca41a4067c08d57dd416687d43
Shamil Khalilov <shamfama@gmail.com>
2020-05-29 21:04:32 +0300

    Update patcher.sh

--- contracts/ethereum/patcher.sh
+++ contracts/ethereum/patcher.sh
@@ -2,6 +2,9 @@
 
 eth_address=''
 
+eth_port='8545'
+eth_network=''
+
 replace_address_in_migration () {
     local migration_name='2_initial_contracts.js'
     local route_to_file="migrations/$migration_name"
@@ -13,17 +16,32 @@ replace_address_in_migration () {
     echo "$updated_file" >> "$route_to_file"
 }
 
-main () {
+update_truffle_config () {
 
+    local template='
+    module.exports = {
+        external: {
+        host: "%s",     // Localhost (default: none)
+        port: 8545,            // Standard Ethereum port (default: none)
+        network_id: "*",       // Any network (default: none)
+        gas: 0,
+        skipDryRun: false
+        }
+    }
+    '
+
+    printf "$template" $eth_network > "truffle-config.js"
+}
+
+main () {
     while [ -n "$1" ]
     do
         case "$1" in
-            --eth-address) eth_address=$2 ;;
+            --eth-address) eth_address=$2; replace_address_in_migration ;;
+            --eth-network) eth_network=$2; update_truffle_config ;;
         esac 
         shift
     done
-
-    replace_address_in_migration
 }
 
 main $@
