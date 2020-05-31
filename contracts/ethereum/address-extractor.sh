ae61a8c61353267f6830b40dabc4e7722d8c5ca3
Shamil Khalilov <shamfama@gmail.com>
2020-05-31 13:53:15 +0300

    Update nebula address extractor.sh

--- contracts/ethereum/address-extractor.sh
+++ contracts/ethereum/address-extractor.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+entered=0
 while read -r in; 
-do 
-    echo "$in" | sed -ne '/Deploying 'Nebula'/,$ p' | grep 'contract address:' | head -n1 | awk '{ print $4 }'
+do     
+    nebula_line=$(echo "$in" | sed -ne "/Deploying \'Nebula\'/,$ p")
+
+    if [ -n "$nebula_line" ]
+    then
+        entered=1
+    fi
+
+    if [ $entered -eq 1 ]
+    then
+        nebula_address=$(echo "$in" | grep 'contract address:' | head -n1 | awk '{ print $4 }')
+
+        if [ -n "$nebula_address" ]
+        then
+            echo "$nebula_address"
+            break
+        fi
+    fi
 done
