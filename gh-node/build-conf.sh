ec72c8eb01fa94515efc70ef4c57c484ba81f4eb
Elshan Dzhafarov (CryptoBadBoy) <raiman.elshan@gmail.com>
2020-06-10 12:31:01 +0300

    fix conf

--- gh-node/build-conf.sh
+++ gh-node/build-conf.sh
@@ -18,8 +18,9 @@ main () {
     "GHNodeURL":"http://localhost:26657",
     "Timeout":"1",
     "NebulaId":"0000000000000000000000000000000000000000000000000000000000000000",
-    "EthNodeUrl":"http://localhost:8545",
-    "NebulaContract":"%s"\n}\n'
+    "NodeUrl":"http://localhost:8545",
+    "NebulaContract":"%s"
+    "ChainType": "Ethereum"\n}\n'
     printf "$JSON_FMT" "$nebula_address" > "$config_name"
 }
 
