69ec2d65c6423e744fa2c3f4f614849d95d7a501
riotpurpp <shamfama@gmail.com>
2020-06-14 17:37:04 +0000

    Update migration var

--- contracts/ethereum/migrations/2_initial_contracts.js
+++ contracts/ethereum/migrations/2_initial_contracts.js
@@ -5,7 +5,7 @@ const Queue = artifacts.require("./libs/QueueLib");
 module.exports = async function(deployer, network, accounts) {
   await deployer.deploy(Queue);
   await deployer.link(Queue, Nebula);
-  let nubula = await deployer.deploy(Nebula, Array(5).fill("ADDRESS"), 1);
+  let nubula = await deployer.deploy(Nebula, ADDRESS, 1);
   let sub = await deployer.deploy(SubMock, nubula.address, "100000000000000");
   await nubula.subscribe(sub.address, 0, "100000000000000")
   await web3.eth.sendTransaction({ from: accounts[0], to: sub.address, value:  "10000000000000000000" });
