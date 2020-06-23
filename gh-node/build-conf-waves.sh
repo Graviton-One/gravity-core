adb47e5b3df7b73dfe1a2900f1bb38f678cb3cab
riotpurpp <shamfama@gmail.com>
2020-06-23 21:22:40 +0000

    Update deps script

--- gh-node/build-conf-waves.sh
+++ gh-node/build-conf-waves.sh
@@ -5,6 +5,7 @@ main () {
     local config_name='config-waves.json'
     local native_url='http://127.0.0.1:26657'
     local node_url='http://0.0.0.0:6869'
+    local priv_key=''
 
     while [ -n "$1" ]
     do
@@ -12,18 +13,20 @@ main () {
             --config) config_name="$2.json" ;;
             --node-url) node_url=$2 ;;
             --native-url) native_url=$2 ;;
+            --priv-key) priv_key=$2 ;;
         esac
         shift
     done
 
     JSON_FMT='{
+    "TCPrivateKey": "%s",   
     "GHNodeURL":"%s",
     "Timeout": 0,
     "NebulaId":"0000000000000000000000000000000000000000000000000000000000000001",
     "NodeUrl":"%s",
     "NebulaContract":"3MHQtdw8myT6b42RD4a9zc346MxgeKeKTkE",
     "ChainType": "Waves"\n}\n'
-    printf "$JSON_FMT" "$native_url" "$node_url" > "$config_name"
+    printf "$JSON_FMT" "$priv_key" "$native_url" "$node_url" > "$config_name"
 }
 
 main $@
