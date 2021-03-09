e3a2c1d989252d1db17b68616212fe7f32b03236
Alexander Kriventsev <alexander.kriventsev@gmail.com>
2021-03-09 15:05:13 +0300

    fix chaini type

--- cmd/gravity/commands/ledger.go
+++ cmd/gravity/commands/ledger.go
@@ -466,7 +466,7 @@ func startLedger(ctx *cli.Context) error {
 func createApp(db *badger.DB, ledgerValidator *account.LedgerValidator, privKeys config.Keys, cfg config.LedgerConfig, genesisCfg config.Genesis, bootstrap string, localHost string, ctx context.Context) (*app.GHApplication, error) {
 	bAdaptors := make(map[account.ChainType]adaptors.IBlockchainAdaptor)
 	for k, v := range cfg.Adapters {
-		chainType, err := account.ParseChainType(k)
+		chainType, err := account.ParseChainType(v.ChainType)
 		if err != nil {
 			return nil, err
 		}
