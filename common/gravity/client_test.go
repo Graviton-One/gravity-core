1b9f510b4d1a94ffcbbb714098fb6597596658ab
Alexander Kriventsev <alexander.kriventsev@gmail.com>
2021-06-15 18:28:09 +0300

    change test

--- common/gravity/client_test.go
+++ common/gravity/client_test.go
@@ -1,33 +1,56 @@
 package gravity
 
 import (
+	"encoding/json"
 	"log"
 	"testing"
 
-	"github.com/Gravity-Tech/gravity-core/common/account"
-	"github.com/Gravity-Tech/gravity-core/common/gravity"
-	"github.com/btcsuite/btcutil/base58"
+	"github.com/Gravity-Tech/gravity-core/common/storage"
+	"github.com/Gravity-Tech/gravity-core/ledger/query"
+	rpchttp "github.com/tendermint/tendermint/rpc/client/http"
 )
 
 func TestClient_do(t *testing.T) {
-	// rq := query.ByNebulaRq{
-	// 	ChainType:     6,
-	// 	NebulaAddress: "4VL4hsSPPNdqP5ajXinJ3L434uycugBxYaJiJ2Zv4FPo",
+	rq := query.ByNebulaRq{
+		ChainType:     6,
+		NebulaAddress: "4VL4hsSPPNdqP5ajXinJ3L434uycugBxYaJiJ2Zv4FPo",
+	}
+	var rqi interface{}
+	rqi = rq
+
+	//ghClient, err := New("http://localhost:26657")
+
+	// if err != nil {
+	// 	log.Print(err)
+	// 	t.FailNow()
 	// }
-	ghClient, err := gravity.New("http://localhost:26657")
-
+	var err error
+	var b []byte
+	b, ok := rqi.([]byte)
+	if !ok {
+		b, err = json.Marshal(rq)
+		if err != nil {
+			log.Print(err)
+			t.FailNow()
+		}
+	}
+	client, err := rpchttp.New("http://localhost:26657", "/websocket")
 	if err != nil {
 		log.Print(err)
 		t.FailNow()
 	}
-	b := base58.Decode("4VL4hsSPPNdqP5ajXinJ3L434uycugBxYaJiJ2Zv4FPo")
-	nid := account.BytesToNebulaId(b)
-	r, err := ghClient.NebulaCustomParams(6, nid)
+
+	rs, err := client.ABCIQuery(string("nebulaCustomParams"), b)
+
+	nebulaCustomParams := storage.NebulaCustomParams{}
+
+	err = json.Unmarshal(rs.Response.Value, &nebulaCustomParams)
 	if err != nil {
 		log.Print(err)
 		t.FailNow()
 	}
 
-	log.Print(r)
+	log.Print("KARAMBA")
+	log.Print(nebulaCustomParams)
 	t.FailNow()
 }
