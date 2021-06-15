e5ec7bd749b58d63c81146f87b60cdf8571ea9a9
Alexander Kriventsev <alexander.kriventsev@gmail.com>
2021-06-15 18:09:03 +0300

    change test

--- common/gravity/client_test.go
+++ common/gravity/client_test.go
@@ -1,29 +1,33 @@
 package gravity
 
 import (
-	"encoding/json"
 	"log"
 	"testing"
 
-	"github.com/Gravity-Tech/gravity-core/ledger/query"
+	"github.com/Gravity-Tech/gravity-core/common/account"
+	"github.com/Gravity-Tech/gravity-core/common/gravity"
+	"github.com/btcsuite/btcutil/base58"
 )
 
 func TestClient_do(t *testing.T) {
-	rq := query.ByNebulaRq{
-		ChainType:     6,
-		NebulaAddress: "4VL4hsSPPNdqP5ajXinJ3L434uycugBxYaJiJ2Zv4FPo",
-	}
-	var rqi interface{}
+	// rq := query.ByNebulaRq{
+	// 	ChainType:     6,
+	// 	NebulaAddress: "4VL4hsSPPNdqP5ajXinJ3L434uycugBxYaJiJ2Zv4FPo",
+	// }
+	ghClient, err := gravity.New("http://localhost:26657")
 
-	rqi = rq
-	var err error
-	b, ok := rqi.([]byte)
-	if !ok {
-		b, err = json.Marshal(rq)
-		if err != nil {
-			t.FailNow()
-		}
+	if err != nil {
+		log.Print(err)
+		t.FailNow()
 	}
-	log.Print(b)
+	b := base58.Decode("4VL4hsSPPNdqP5ajXinJ3L434uycugBxYaJiJ2Zv4FPo")
+	nid := account.BytesToNebulaId(b)
+	r, err := ghClient.NebulaCustomParams(6, nid)
+	if err != nil {
+		log.Print(err)
+		t.FailNow()
+	}
+
+	log.Print(r)
 	t.FailNow()
 }
