6be480feba6d76846a7d165eee8d0f846a41af0d
Alexander Kriventsev <alexander.kriventsev@gmail.com>
2021-06-15 18:38:35 +0300

    update test

--- common/gravity/client_test.go
+++ common/gravity/client_test.go
@@ -10,7 +10,7 @@ import (
 	rpchttp "github.com/tendermint/tendermint/rpc/client/http"
 )
 
-func TestClient_do(t *testing.T) {
+func TestNebulaInfo(t *testing.T) {
 	rq := query.ByNebulaRq{
 		ChainType:     6,
 		NebulaAddress: "4VL4hsSPPNdqP5ajXinJ3L434uycugBxYaJiJ2Zv4FPo",
@@ -18,12 +18,6 @@ func TestClient_do(t *testing.T) {
 	var rqi interface{}
 	rqi = rq
 
-	//ghClient, err := New("http://localhost:26657")
-
-	// if err != nil {
-	// 	log.Print(err)
-	// 	t.FailNow()
-	// }
 	var err error
 	var b []byte
 	b, ok := rqi.([]byte)
@@ -43,6 +37,54 @@ func TestClient_do(t *testing.T) {
 	}
 
 	rs, err := client.ABCIQuery(string("nebula_info"), b)
+	if err != nil {
+		log.Print("KARAMBA 3")
+		log.Print(err)
+		t.FailNow()
+	}
+
+	nebulaCustomParams := storage.NebulaCustomParams{}
+	log.Print("RESULT")
+	log.Print(rs.Response.Value)
+	err = json.Unmarshal(rs.Response.Value, &nebulaCustomParams)
+	if err != nil {
+		log.Print("KARAMBA 4")
+		log.Print(err)
+		t.FailNow()
+	}
+
+	log.Print("KARAMBA 5")
+	log.Print(nebulaCustomParams)
+	t.FailNow()
+}
+
+func TestNebulaCustomParams(t *testing.T) {
+	rq := query.ByNebulaRq{
+		ChainType:     6,
+		NebulaAddress: "4VL4hsSPPNdqP5ajXinJ3L434uycugBxYaJiJ2Zv4FPo",
+	}
+	var rqi interface{}
+	rqi = rq
+
+	var err error
+	var b []byte
+	b, ok := rqi.([]byte)
+	if !ok {
+		b, err = json.Marshal(rq)
+		if err != nil {
+			log.Print("KARAMBA 1")
+			log.Print(err)
+			t.FailNow()
+		}
+	}
+	client, err := rpchttp.New("http://localhost:26657", "/websocket")
+	if err != nil {
+		log.Print("KARAMBA 2")
+		log.Print(err)
+		t.FailNow()
+	}
+
+	rs, err := client.ABCIQuery(string("nebulaCustomParams"), b)
 	if err != nil {
 		log.Print("KARAMBA 3")
 		log.Print(err)
